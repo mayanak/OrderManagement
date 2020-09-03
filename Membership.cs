b394c7cb66b4098eb840cf5ed98f092871d3ccc9
mayanak <45207952+mayanak@users.noreply.github.com>
2020-09-03 17:10:40 +0530

    Update Membership.cs

--- Membership.cs
+++ Membership.cs
@@ -1,1 +1,23 @@
 
+using System;
+
+namespace PaymentGateWay
+{
+     public class MemberShip :IMemberShipRepository
+     {
+        public void ActivateMember()
+        {
+        // To do need to added properties to add the the member
+           console.Writeline(" Member added sucessfully");
+        }
+        
+       public void GetCommisionDetails()
+       {
+            console.Writeline(" Book Order commision percentage is 4%");
+       }
+       
+     }
+     
+     
+    
+}
