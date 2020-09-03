58b3cf0aea4d83d9fcd2256067734a27cb651ff3
mayanak <45207952+mayanak@users.noreply.github.com>
2020-09-03 16:19:12 +0530

    Program.cs file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OrderManagement
+{
+    public class Program
+    {
+        public static void Main()
+        {
+          string paymentFor;
+          console.WriteLine(" Please enter product type");
+          paymentFor=console.ReadLine();
+          
+           
+        }
+    }
+}
