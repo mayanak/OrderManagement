738e62ee5121a90d1a3c87912dc2bed586628f37
mayanak <45207952+mayanak@users.noreply.github.com>
2020-09-03 17:26:29 +0530

    Update OrderManagementTest.cs

--- OrderManagementTest.cs
+++ OrderManagementTest.cs
@@ -1,1 +1,19 @@
+using Microsoft.VisuvalStudio.TestingTools.UnitTesting;
+using PayamentGateWay;
 
+namespace UnitTestProject
+{
+[TestClass]
+
+public class UnitTest1
+{
+[TestMethod]
+  public void GenrateOderPaySlip()
+  {
+       PaymentAbstract objAbstract= new ConcreateFactory();
+       
+       
+  }
+}
+
+}
