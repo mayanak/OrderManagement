92bbddd98ae03a52cdabdcdbd129d966b00b9409
mayanak <45207952+mayanak@users.noreply.github.com>
2020-09-03 17:07:26 +0530

    Update PhysicalProduct.cs

--- PhysicalProduct.cs
+++ PhysicalProduct.cs
@@ -1,1 +1,22 @@
 
+using System;
+
+namespace PaymentGateWay
+{
+     public class PhysicalProduct :IPaymentRepository
+     {
+        public string GeneratePayslip()
+        {
+          return "Physical order payslip generaed ";
+        }
+        
+       public void GetCommisionDetails()
+       {
+            console.Writeline(" Book Order commision percentage is 10%");
+       }
+       
+     }
+     
+     
+    
+}
