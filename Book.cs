7071d5a05bbe4ce017c403013fd8c57d8d45a415
mayanak <45207952+mayanak@users.noreply.github.com>
2020-09-03 17:06:07 +0530

    Update Book.cs

--- Book.cs
+++ Book.cs
@@ -1,1 +1,22 @@
 
+using System;
+
+namespace PaymentGateWay
+{
+     public class Book :IPaymentRepository
+     {
+        public string GeneratePayslip()
+        {
+          return "Book order payslip generaed ";
+        }
+        
+       public void GetCommisionDetails()
+       {
+            console.Writeline(" Book Order commision percentage is 4%");
+       }
+       
+     }
+     
+     
+    
+}
